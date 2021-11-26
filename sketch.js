47b393c2f84168a11cdb36c1fd1a643c1d5064e6
ocello3 <tkaa.2160@gmail.com>
2021-11-26 19:10:43 +0900

    enable logo motion

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 let params;
+const canvasDiv = document.getElementById('sketch'); 
 
 const testSetup = () => {
 }
 
 const thisSetup = s => {
-	const canvasDiv = document.getElementById('sketch');
+	
 	params = {
 		width: canvasDiv.clientWidth,
 		height: canvasDiv.clientWidth*0.2,
+		scrollTop: 0,
+		scrollmax: 200,
 	};
 	s.createCanvas(params.width, params.height);
 	testSetup();
@@ -18,19 +21,24 @@ const testDraw = () => {
 }
 
 const thisDraw = s => {
-	/* frame
 	s.background(255);
+	/* frame
 	s.push();
 	s.noFill();
 	s.stroke(0);
 	s.rect(0, 0, params.width, params.height);
 	s.pop(); */
+	// update
+	params.scrollTop = canvasDiv.scrollTop || document.body.scrollTop || window.pageYOffset;
+	const multRate = (params.scrollmax - params.scrollTop) / params.scrollmax;
 	// font
 	s.textSize(34);
 	s.textFont('Helvetica Neue');
 	s.textAlign(s.LEFT, s.CENTER);
-	s.text('Sketch List',0, params.height/2);
-	testDraw();
+	s.scale(1, multRate);
+	s.fill(255 - 255 * multRate);
+	s.text('Sketch List', 0, params.height/2);
+	// testDraw();
 }
 
 const sketch = s => {
