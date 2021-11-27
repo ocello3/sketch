566dae40b4ce0466300220fbd363a3a604cb98cd
ocello3 <tkaa.2160@gmail.com>
2021-11-27 21:24:07 +0900

    2021-11-27

--- 2021/11/27/params.js
+++ 2021/11/27/params.js
@@ -1,20 +1,28 @@
 import { s } from './sketch.js';
 
-export const setParams = () => {
-	const canvasDiv = document.getElementById('sketch');
-	return {
-		frameCount: 0,
-		frameRate: 0,
-		size: canvasDiv.clientWidth,
+export const setParams = (params) => {
+	// scroll setting for ios safari
+	window.onscroll = function () {
+		if (document.body.scrollHeight == window.innerHeight + window.pageYOffset) {
+			window.focus();
+		}
 	}
-}
-
-export const updateParams = (params) => {
-	params.frameCount += 1;
-	params.frameRate = s.frameRate();
+	// calc params
+	params.canvasDiv = document.getElementById('sketch');
+	params.size = params.canvasDiv.clientWidth;
+	params.scrollTop = 0;
+	params.scrollmax = 200;
+	// font info
+	params.fontSize = 1;
+	params.text = 'Sketch List';
+	params.points = params.font.textToPoints(params.text, 0, 0, params.fontSize, { // x, y - coordinate
+		sampleFactor: 5, // higher values yield more points
+		simplifyThreshold: 0, // if set to a non-zero value, collinear points will be be removed from the polygon
+	});
+	params.bounds = params.font.textBounds(params.text, 0, 0, params.fontSize);
 }
 
 export const gui = params => {
 	const pane = new Tweakpane.Pane({ container: document.getElementById('pane') });
-	pane.addMonitor(params, 'frameRate', {interval: 500});
+	pane.addMonitor(params, 'scrollTop', {interval: 500});
 }
--- 2021/11/27/sketch.js
+++ 2021/11/27/sketch.js
@@ -1,15 +1,21 @@
-import { setParams, updateParams, gui } from './params.js';
-// import { setDot, updateDot } from './dot.js';
+import { setParams, gui } from './params.js';
+import { drawOutline } from './outline.js';
 
 let params;
+
+const thisPreload = s => {
+	params = {
+		font: s.loadFont('../../../font/HelveticaNeue-Regular.otf'),　
+	}
+}
 
 const testSetup = () => {
 }
 
 const thisSetup = s => {
-	params = setParams();
-	s.createCanvas(params.size, params.size);
+	setParams(params);
 	gui(params);
+	s.createCanvas(params.size, params.size);
 	testSetup();
 }
 
@@ -19,23 +25,34 @@ const testDraw = () => {
 
 const thisDraw = s => {
 	s.background(255);
-	// frame
+	/* frame
 	s.push();
 	s.noFill();
 	s.stroke(0);
-	s.rect(0, 0, params.size);
-	s.pop();
-	// draw text
+	s.rect(0, 0, params.size, params.size);
+	s.pop(); */
+	// update
+	params.scrollTop = params.canvasDiv.scrollTop || document.body.scrollTop || window.pageYOffset;
+	const multRate = (params.scrollmax - params.scrollTop) / params.scrollmax; // top: 1, bottom: 0
+	// scroll pos
+	s.scale(1, multRate);
+	s.fill(255 - 255 * multRate);
+	// font
+	/*
 	s.push();
-	s.noFill();
-	s.stroke(0);
+	s.textSize(34);
+	s.textFont(params.font);
 	s.textAlign(s.LEFT, s.CENTER);
-	s.textSize(params.size*0.2);
-	s.text('Sketch List', 0, params.size/2)
+	s.text(params.text, 0, params.size/2);
 	s.pop();
+	*/
+	// draw font points
+	drawOutline(params);
+	// testDraw();
 }
 
 const sketch = s => {
+	s.preload = () => thisPreload(s);
 	s.setup = () => thisSetup(s);
 	s.draw = () => thisDraw(s);
 }
