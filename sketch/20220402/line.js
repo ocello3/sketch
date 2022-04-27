0d192bdc81e5338358000c5c2339d1011afc451d
ocello3 <ocello3@users.noreply.github.com>
2022-04-28 07:07:15 +0900

    20220402 pg is working

--- sketch/20220402/line.js
+++ sketch/20220402/line.js
@@ -4,6 +4,7 @@ const setParams = (params) => {
 	line.pgHeight = params.size/2;
 	line.widthRate = 0.1;
 	line.num = Math.floor(line.pgWidth * line.widthRate);
+	line.layer = 2;
 	params.line = line;
 }
 
@@ -12,7 +13,7 @@ const setLine = (index) => (params, s, widths) => {
 	line.width = widths[index];
 	line.height = params.line.pgHeight;
 	const calcXPos = () => {
-		const accWidths = widths.slice(0, index + 1);
+		const accWidths = widths.slice(0, index);
 		return accWidths.reduce((pre, cur) => pre + cur, 0);
 	}
 	line.pos = s.createVector(calcXPos(), 0);
@@ -20,19 +21,41 @@ const setLine = (index) => (params, s, widths) => {
 	return line;
 }
 
-export const setLines = (params, s) => {
+const setPgs = (params, pgs, s) => {
+	const pg = s.createGraphics(params.line.pgWidth, params.line.pgHeight);
+	const pgArray = [pg, pg];
+	pgs.lines = {
+		above: pgArray,
+		below: pgArray,
+	}
+}
+
+export const setLines = (params, pgs, s) => {
 	setParams(params);
+	setPgs(params, pgs, s);
 	const widths = Array.from(Array(params.line.num), () => params.line.pgWidth/params.line.num);
 	return Array.from(Array(params.line.num), (_, index) => setLine(index)(params, s, widths));
 }
 
-export const drawLines = (s, lines) => {
+const drawLines = (pg, lines) => {
 	lines.forEach(line => {
-		s.push();
-		s.noStroke();
-		s.fill(line.color);
-		s.rect(line.pos.x, line.pos.y, line.width, line.height);
-		s.pop();
+		pg.push();
+		pg.noStroke();
+		pg.fill(line.color);
+		pg.rect(line.pos.x, line.pos.y, line.width, line.height);
+		pg.pop();
+	});
+	return false;
+}
+
+export const drawLinePgs = (params, pgs, lines, s) => {
+	pgs.lines.above.forEach((pg) => {
+		drawLines(pg, lines);
+		s.image(pg, 0, 0);
+	});
+	pgs.lines.below.forEach((pg) => {
+		drawLines(pg, lines);
+		s.image(pg, 0, params.size/2);
 	});
 	return false;
 }
--- sketch/20220402/sketch.js
+++ sketch/20220402/sketch.js
@@ -1,26 +1,27 @@
 import { setParams, updateParams, gui } from '../../util/params.js';
 import { drawFrame } from '../../util/drawFrame.js';
 import { debug } from '../../util/debug.js'; // obj, length=null, start=0
-import { setLines, drawLines } from './line.js';
+import { setLines, drawLinePgs } from './line.js';
 // import { setSynth, playSynth } from './synth.js';
 
 const sketch = s => {
 	let params; // size
 	let lines;
+	let pgs = {};
 	// let synth;
 	s.setup = () => {
 		params = setParams();
 		s.createCanvas(params.size, params.size);
 		// tab.pages[0].addInput(params, 'margin');
 		const tab = gui(s, params, false, false); // audio, seq
-		lines = setLines(params, s);
+		lines = setLines(params, pgs, s);
 		// synth = setSynth();
 		s.noLoop();
 		// s.frameRate(2);
 	};
 	s.draw = () => {
 		s.background(255);
-		drawLines(s, lines);
+		drawLinePgs(params, pgs, lines, s);
 		debug(lines, params.line.num);
 		drawFrame(s, params);
 		updateParams(s, params);
