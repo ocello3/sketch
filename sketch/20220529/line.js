a728c88bc626a624723bee6a3fb4ed9786ce75e6
ocello3 <ocello3@users.noreply.github.com>
2022-05-29 14:05:42 +0900

    20220529 init

--- sketch/20220529/line.js
+++ sketch/20220529/line.js
@@ -1,60 +1,24 @@
-export const setLineParams = (params, tab) => {
+const setParams = (params, tab) => {
 	params.line = {
-		interval: 2.16,
-		gapRate: 7.36,
-		weight: 0.5,
-		alpha: 200,
 	}
-	tab.pages[1].addInput(params.line, 'interval', { min: 0, max: 5 });
-	tab.pages[1].addInput(params.line, 'gapRate', { min: 0, max: 20 });
-	tab.pages[1].addInput(params.line, 'weight', { min: 0, max: 2 });
-	tab.pages[1].addInput(params.line, 'alpha', { min: 100, max: 255 });
+	const _param = params.line;
+	const _tab = tab.pages[1];
+	// _tab.addInput(_param, 'interval', { min: 0, max: 5 }); /
 	return false;
 }
 
-export const calcLineObj = (preLineObj, params, s) => {
-	const newLineObj = { ...preLineObj };
+const calc = (preObj, params, s) => {
+	const newObj = { ...preObj };
 	const isInit = (s.frameCount === 0);
+	// const { interval, gapRate } = params.line; /
 	if (isInit) {
-		
-		// preLineObj.lines = Array.from(Array(newLineObj.num), () => 1);
 	}
-	newLineObj.num = Math.floor(params.size / params.line.interval);
-	newLineObj.num = Math.floor(params.size / params.line.interval);
-	newLineObj.lines = Array.from(Array(newLineObj.num), (_, lineIndex) => {
-		const newLine = {};
-		newLine.startPos = (() => {
-			const x = params.line.interval * lineIndex - params.line.gapRate;
-			const y = 0;
-			return s.createVector(x, y);
-		})();
-		newLine.endPos = (() => {
-			const x = params.line.interval * lineIndex + params.line.gapRate;
-			const y = params.size;
-			return s.createVector(x, y);
-
-		})();
-		return newLine;
-	});
-	return newLineObj;
+	return newObj;
 }
 
-export const drawLine = (lineObj, params, s) => {
+const draw = (obj, params, s) => {
 	s.push();
-	s.blendMode(s.MULTIPLY);
-	for (const line of lineObj.lines) {
-		s.strokeWeight(params.line.interval * params.line.weight);
-		s.stroke(111, 208, 140, params.line.alpha);
-		s.line(line.startPos.x, line.startPos.y, line.startPos.x, line.endPos.y);
-		s.stroke(123, 158, 168, params.line.alpha);
-		s.line(line.startPos.x, line.startPos.y, line.endPos.x, line.endPos.y);
-		s.stroke(205, 223, 160, params.line.alpha);
-		s.curve(
-			line.startPos.x, line.endPos.y * 0.5,
-			line.startPos.x, line.startPos.y,
-			line.endPos.x, line.endPos.y,
-			line.endPos.x, line.endPos.y * 0.5
-			);
-	}
 	s.pop();
 }
+
+export const line = { setParams, calc, draw }
--- sketch/20220529/sketch.js
+++ sketch/20220529/sketch.js
@@ -1,7 +1,7 @@
 import { setParams, updateParams, gui } from '../../util/params.js';
 import { drawFrame } from '../../util/drawFrame.js';
 import { debug } from '../../util/debug.js'; // obj, length=null, start=0
-import { setLineParams, calcLineObj, drawLine } from './line.js';
+import { line } from './line.js';
 // import { setSynth, playSynth } from './synth.js';
 
 const sketch = s => {
@@ -13,7 +13,7 @@ const sketch = s => {
 		s.createCanvas(params.size, params.size);
 		const tab = gui(s, params, false, false); // audio, seq
 		// tab.pages[0].addInput(params, 'margin');
-		setLineParams(params, tab);
+		line.setParams(params, tab);
 		// synth = setSynth(params, tab);
 		s.noLoop();
 		// s.frameRate(10);
@@ -21,12 +21,12 @@ const sketch = s => {
 	s.draw = () => {
 		s.background(255);
 		// text
-		lineObj = calcLineObj(lineObj, params, s);
-		drawLine(lineObj, params, s);
+		lineObj = line.calc(lineObj, params, s);
+		line.draw(lineObj, params, s);
 		// debug(lineObj);
 		drawFrame(s, params);
 		updateParams(s, params);
-		// playSynth(textObj, checkboxObj, synth, params, s);
+		// playSynth(lineObj, synth, params, s);
 	};
 }
 new p5(sketch, 'sketch');
