e0f2f441fce0cc3de93489dbd73a08170002ab01
ocello3 <ocello3@users.noreply.github.com>
2026-02-12 19:58:00 +0900

    updated json list

--- component/footer.js
+++ component/footer.js
@@ -1,7 +1,7 @@
 const footerTemplate = () => {
 	return `
 	<ul class="uk-breadcrumb">
-		<li><span>&copy; 2022 ocello3</span></li>
+		<li><span>&copy; 2026 ocello3</span></li>
 		<a href="https://mobile.twitter.com/ocello3" target="_blank" rel="noopener noreferer" class="uk-icon-link" uk-icon="twitter"></a>
 		<a href="https://github.com/ocello3/sketch" target="_blank" rel="noopener noreferer" class="uk-icon-link" uk-icon="github"></a>
 	</ul>
