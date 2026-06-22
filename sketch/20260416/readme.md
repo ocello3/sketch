81b3caa7175d3efc774d1f0f1e0e25f03cd19b8e
ocello3 <ocello3@users.noreply.github.com>
2026-06-22 12:51:40 +0900

    spiraled working

--- sketch/20260416/readme.md
+++ sketch/20260416/readme.md
@@ -4,12 +4,19 @@ wavのloopでeq、音量、panで調整
 物理シミュレートではなく、形と動きの単純化
 wavの解析を描画にフィードバック
 
+回転するらせん
+フェルマー螺旋：r = a * sqrt (theta)
+縦に短い長方形と長い長方形
+長方形の高さはFFT
+gainは3点の合計が-60〜0。一定時間は固定
+
 # todo
-eqをかけてみて、tweakpaneで操作する - gain
-eqをかけてみて、tweakpaneで操作する - freq - 範囲をchat gptで調べる
-動きを考える
+fftのコードを書く
+
 
 # done
 wavを再生するコードを書く
 再生するwavをきめる
 wavが再生できるか確認する
+eqをかけてみて、tweakpaneで操作する - gain
+eqをかけてみて、tweakpaneで操作する - freq - 対数変換
--- sketch/20260416/sketch.js
+++ sketch/20260416/sketch.js
@@ -16,11 +16,13 @@ const sketch = (s) => {
 		snd.eq_1 = new p5.EQ(3);
 		snd.file_1.disconnect();
 		snd.eq_1.process(snd.file_1);
+		snd.fft_1 = new p5.FFT();
 		const f = u.createPane(s, p, () => {
 			// (activate) snd.osc.start());
 		});
 		const f1 = f.addFolder({ title: "sketch" });
 		const f2 = f.addFolder({ title: "sound" });
+		// gain
 		f2.addBinding(p, 'eq_1_low_gain', {
 			min: -20,
 			max: 20,
@@ -39,6 +41,28 @@ const sketch = (s) => {
 		}).on('change', (ev) => {
 			snd.eq_1.bands[2].gain(ev.value);
 		});
+		// freq
+		f2.addBinding(p, 'eq_1_low_freq', {
+			min: 0,
+			max: 1,
+		}).on('change', (ev) => {
+			const freq = Math.pow(10, s.map(ev.value, 0, 1, Math.log10(50), Math.log10(1000)));
+			snd.eq_1.bands[0].gain(ev.value);
+		});
+		f2.addBinding(p, 'eq_1_mid_freq', {
+			min: 0,
+			max: 1,
+		}).on('change', (ev) => {
+			const freq = Math.pow(10, s.map(ev.value, 0, 1, Math.log10(200), Math.log10(5000)));
+			snd.eq_1.bands[0].gain(ev.value);
+		});
+		f2.addBinding(p, 'eq_1_high_freq', {
+			min: 0,
+			max: 1,
+		}).on('change', (ev) => {
+			const freq = Math.pow(10, s.map(ev.value, 0, 1, Math.log10(1000), Math.log10(15000)));
+			snd.eq_1.bands[0].gain(ev.value);
+		});
 	};
 	s.draw = () => {
 		function updateSnd() {}
@@ -46,12 +70,22 @@ const sketch = (s) => {
 
 		function getDt(_dt) {
 			let dt = { ..._dt };
+			dt.spectrum = snd.fft_1.analyze();
+			// Fermat spiral : r = a * sqrt (theta)
+			dt.barSizes = dt.spectrum.map(vol => s.map(vol, 0, 255, 0, size * p.barSizeRate));
+			dt.angles = dt.barSizes.map((_, i) => s.map(i, 0, dt.barSizes.length, 0, 2 * Math.PI * p.laps));
+			dt.poses = dt.angles.map((angle) => {
+				const r = p.radiusRate * size * Math.sqrt(angle);
+				const x = Math.cos(angle) * r + size * 0.5;
+				const y = Math.sin(angle) * r + size * 0.5;
+				return s.createVector(x, y);
+			});
 			return dt;
 		}
 		dt = getDt(dt);
 		s.background(255);
 		u.drawFrame(s, size);
-		u.debug(s, p, p, 2);
+		u.debug(s, p, dt, 5);
 		p.frameRate = s.isLooping() ? s.frameRate() : 0;
 
 		function playSnd() {
@@ -60,7 +94,12 @@ const sketch = (s) => {
 		}
 		playSnd();
 
-		function drawDt() {}
+		function drawDt() {
+			dt.poses.forEach((pos, i) => {
+				s.fill(0);
+				s.rect(pos.x, pos.y, 2, dt.barSizes[i]);
+			});
+		}
 		drawDt();
 
 		// itit params
